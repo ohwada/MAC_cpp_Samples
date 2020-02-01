44387ee184c27275f3da4415d5ebc4ff460d7e1b
Kenichi Ohwada <ml.ohwada@gmail.com>
2020-02-01 09:58:58 +0900

    update

--- OpenCV-ImageReadWrite/README.md
+++ OpenCV-ImageReadWrite/README.md
@@ -7,14 +7,16 @@ read and write Image File <br/>
 ### install OpenCV
 % brew install opencv <br/>
 
+### build sample code 
+% g++  image_read_write.cpp parse_filename.cpp -o image_read_write -std=c++11 `pkg-config --cflags --libs opencv4` <br/>
+
 ### excute sample code 
-% g++  image_read_write.cpp -o image_read_write -std=c++11 `pkg-config --cflags --libs opencv4` <br/>
-% ./image_read_write <br/>
+% ./image_read_write ./images/vegetables.jpg <br/>
 
 ### result 
-read image file <br/>  
+read image file  <br/>  
 ex ) vegetables.jpg <br/>
-and show image <br/>
+and show image  <br/>
 <image src="https://raw.githubusercontent.com/ohwada/MAC_cpp_Samples/master/OpenCV-ImageReadWrite/result/screenshot_imshow.png" width="300" /><br/>
 
 and write output file  <br/>
--- OpenCV-ImageReadWrite/image_read_write.cpp
+++ OpenCV-ImageReadWrite/image_read_write.cpp
@@ -7,24 +7,10 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
+#include "parse_filename.hpp"
 
 using namespace cv;
 using namespace std;
-
-/**
- * parseFileName
- */
-bool parseFileName(string name, string &title, string &ext)
-{
-    size_t pos = name.find('.');
-    if(pos == string::npos){
-        return false;
-    }
-
-    title = name.substr(0, pos);
-    ext = name.substr(pos+1, name.size()-pos);
-    return true;
-}
 
 
 /**
@@ -33,31 +19,29 @@ bool parseFileName(string name, string &title, string &ext)
 int main(int argc, char** argv)
 {
 
-    cv::CommandLineParser parser(argc, argv,
-        "{input|vegetables.jpg|input file}"
-    );
-
-    // get input filename
-    std::string input;
-    if (parser.has("input"))
-    {
-        input = parser.get<string>("input");
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <inputImageFile> "  << endl;
+        return 1;
     }
 
+    // specify filename from commandline 
+    string input = argv[1];
+
     // create output file name
+    string dir;
     string title;
     string ext;
-    parseFileName(input, title, ext);
+    parseFileName(input, dir, title, ext);
 
     string output = title + "_gray." + ext;
 
     //load as color image
     Mat color = imread(input, 1);
 
-
     if(color.empty())
     {
         // finish the program, if not load the image
+        cerr << "read faild: " << input << endl;
         return 1;
     }
 
@@ -67,6 +51,7 @@ int main(int argc, char** argv)
 
     // save to output file
     imwrite(output, gray);
+    cerr << "write to: " << output << endl;
 
     // show input image
     cv::namedWindow(input, WINDOW_AUTOSIZE|WINDOW_FREERATIO);
