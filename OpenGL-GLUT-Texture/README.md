75fdf0c3e439ffe35f2e2771298f69c9a1061ff0
Kenichi Ohwada <ml.ohwada@gmail.com>
2020-03-21 03:28:27 +0900

    update

--- OpenGL-GLUT-Texture/README.md
+++ OpenGL-GLUT-Texture/README.md
@@ -53,32 +53,27 @@ and draw  Texture with CheckerBoard pattern <br/>
 
 
 ## image_texture.cpp <br/>
-  % g++ image_texture.cpp texture_util.cpp TGALoader.cpp parse_filename.cpp -o image -std=c++11 -framework OpenGL -framework GLUT 
+  % g++ image_texture.cpp texture_util.cpp parse_filename.cpp -o image -std=c++11 -framework OpenGL -framework GLUT 
 
 ### run sample code
 Usage: image \< inputImageFile \> <br/>
 ex) <br/>
- % ./image ./images/upsidedown.tga
+ % ./image ./images/park.tga
 
 ### result 
 display as below <br/>
 OpenGL/GLUT Texture <br/>
-Usage: ./image \< inputImageFile \> <br/> 
-OpenGL Vender: Intel Inc. <br/>
+loadImage: ./images/park.tga ( 512 x 512 ) <br/>
 
 
 open OpenGL window <br/>
 and read TGA file <br/>
 and draw  Texture <br/>
-<image src="https://raw.githubusercontent.com/ohwada/MAC_cpp_Samples/master/OpenGL-GLUT-Texture/result/screenshot_upsidedown.png" width="300" /><br/>
-
-NOTE : <br/>
-it will be upside down,  <br/>
-when mapping the image on the Texture <br/>
+<image src="https://raw.githubusercontent.com/ohwada/MAC_cpp_Samples/master/OpenGL-GLUT-Texture/result/screenshot_park.png" width="300" /><br/>
 
 
 ### Reference <br/>
 - https://www.opengl.org/
 - http://www.openspc2.org/format/TGA/index.html
-- http://www.oit.ac.jp/is/L231/pukiwiki/index.php?%E6%84%9F%E8%A6%9A%E3%83%A1%E3%83%87%E3%82%A3%E3%82%A2%E7%A0%94%E7%A9%B6%E5%AE%A4/OpenGL/%E3%83%86%E3%82%AF%E3%82%B9%E3%83%81%E3%83%A3%E3%83%9E%E3%83%83%E3%83%94%E3%83%B3%E3%82%B0#s205ba9e
+- http://asura.iaigiri.com/OpenGL/gl5.html
 
--- OpenGL-GLUT-Texture/image_texture.cpp
+++ OpenGL-GLUT-Texture/image_texture.cpp
@@ -6,27 +6,62 @@
 
 #include "texture_util.hpp"
 
-#include "TGALoader.hpp"
-
 #include "parse_filename.hpp"
 
 using namespace std;
 
 
-/**
- * createWindowTitle
+/*
+ * readImage
  */
-string createWindowTitle(string input)
+char* readImage(string input, int &width, int &height)  
 {
 
-    string dir;
-    string title;
-    string ext;
-    parseFileName(input, dir, title, ext);
+    char header[18]; 
 
-    string window_title =  "Image Texture: " + title;
+  /* texture file open */  
+    FILE *fp = fopen(input.c_str(), "rb");
+    if(fp==NULL){  
+	    cerr << "Could not open file : " << input << endl;
+        return NULL;  
+    } 
 
-    return window_title;
+
+	// read header information
+    int num = sizeof(header);
+	int ret = fread(header, 1, num, fp);
+    if(ret < num){
+		cerr << "Could not read header : " << input << endl;
+		return NULL;
+    }
+
+
+	// determine width and height
+	width = header[13] * 256 + header[12];
+	height = header[15] * 256 + header[14];
+
+
+	// create image buffer
+    int bufsize = 4*width*height;
+    char *image = new char[bufsize];
+
+    // tga order B G R A
+    // image order R G B A
+
+  for(int y=0;y<height;y++){  
+    for(int x=0;x<width;x++){ 
+        int index = 4*width*y + 4*x; 
+      image[index+2] = fgetc(fp); /* B */  
+      image[index+1] = fgetc(fp); /* G */  
+      image[index+0] = fgetc(fp); /* R */  
+      image[index+3] = fgetc(fp); /* alpha */  
+    } // x 
+  } //y
+
+
+  fclose(fp); 
+
+    return image; 
 }
 
 
@@ -36,7 +71,6 @@ string createWindowTitle(string input)
 int main(int argc , char ** argv) 
 {
 
-    TGAImage tgaImage; 
     const int WIN_WIDTH = 500;
     const int WIN_HEIGHT = 500;
     const int WIN_POS_X = 100;
@@ -51,8 +85,12 @@ int main(int argc , char ** argv)
 
     string  input = argv[1];
 
-    string win_title = createWindowTitle(input);
-            
+    string fname = getFileNameWithExt(input);
+
+    string win_title =  "Image Texture: " + fname;
+
+ 
+    // open window           
   glutInit(&argc, argv); 
 
     setupWindow(win_title,
@@ -61,19 +99,21 @@ int main(int argc , char ** argv)
         WIN_POS_X, 
         WIN_POS_Y );
 
-	bool ret = tgaImage.ReadTGA(input);
-    if(!ret){
-        cerr << "Usage: " <<  "ReadTGA Faild "  << endl;
+// read Image
+    int width;
+    int height;
+	char* image = readImage(input, width, height);
+    if(!image){
+        cerr <<  "readImage Faild "  << input << endl;
         return EXIT_FAILURE;
     }
 
-	GLubyte* image = tgaImage.getImageData();
-	GLuint width = tgaImage.getWidth();
-	GLuint height = tgaImage.getHeight();
-	GLuint internalFormat = tgaImage.getInternalFormat();
-	GLenum format = tgaImage.getFormat();
+    // file info
+    string str_size = " ( " + to_string(width)  + " x " + to_string(height) + " )";
+    cout << "loadImage: " << input  << str_size << endl;
 
-    setupTexture(width, height, internalFormat, format, image);
+    // show in window
+    setupTexture((GLubyte*)image, width, height, GL_RGBA, GL_RGBA);
 
     cout << "Texture Successfully" << endl;
 
