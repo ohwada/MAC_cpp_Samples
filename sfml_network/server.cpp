0c65c0a697f41aee769fe4ff53da9f2ed8755a50
Kenichi Ohwada <ml.ohwada@gmail.com>
2020-08-02 12:17:29 +0900

    update

--- sfml_network/server.cpp
+++ sfml_network/server.cpp
@@ -18,23 +18,18 @@
 
 #include "sfml_status.hpp"
 #include "host.h"
+#include "string_upper.hpp"
+
 
 using namespace std;
 
+// prototype
+int procPacket(sf::TcpSocket* socket);
 
-/**
- * toUpper
- */
-char toUpper(char c) {
-    char ret;
-    if(isalpha(c)) {
-        ret = toupper(c);
-    } else {
-        ret = c;
-    }
-    return ret;
-}
-
+// global
+const int CODE_DONE = 0;
+const int CODE_DISCONNECTED = 1;
+const int CODE_ERROR = 2;
 
 
 /**
@@ -60,7 +55,6 @@ int main(int argc, char **argv)
     cout << "waiting for connection port: " << PORT << endl;
 
     sf::IpAddress clientAddress;
-    sf::TcpSocket client;
 
 while(1)
 {
@@ -72,7 +66,8 @@ while(1)
     cout << "or quit with ctrl-c" << endl;
 
     // accept a new connection
-    status = listener.accept(client) ;
+    sf::TcpSocket socket;
+    status = listener.accept(socket) ;
     if(status != sf::Socket::Done)
     {
         cout << "accept failed" << endl;
@@ -81,27 +76,53 @@ while(1)
     }
 
     // connected
-    sf::IpAddress client_addr = client.getRemoteAddress ();
+    sf::IpAddress client_addr = socket.getRemoteAddress ();
  
-    unsigned short client_port = 	client.getRemotePort ();
+    unsigned short client_port = 	socket.getRemotePort ();
  
     cout << "connected from: " <<  client_addr << " : " <<  client_port << endl;
 
+        int ret = procPacket(&socket);
+        if (ret == CODE_DISCONNECTED) {
+                continue;
+        } else if (ret == CODE_ERROR) {
+                return  EXIT_FAILURE;
+        }
+
+    } // while
+
+    return EXIT_SUCCESS;
+}
+
+
+/**
+ * procPacket
+ *  Note:
+ *  copy constructor of 'Socket' is implicitly deleted 
+ * because base class 'sf::NonCopyable' has an inaccessible copy
+ */
+int procPacket(sf::TcpSocket* socket)
+{
+
+    sf::Socket::Status status;
     sf::Packet packet_recv;
     sf::Packet packet_send;
     string str_recv;
     string str_send;
+    int ret = CODE_DONE;
 
     while(1)
     {
         // recieve packet
-        status = client.receive(packet_recv);
+        status = (*socket).receive(packet_recv);
         if (status == sf::Socket::Disconnected) {
             printStatus(status);
+            ret = CODE_DISCONNECTED;
             break;
         } else if (status != sf::Socket::Done) {
             printStatus(status);
-            return  EXIT_FAILURE;
+            ret = CODE_ERROR;
+            break;
         }
 
         if (packet_recv >> str_recv)
@@ -109,24 +130,26 @@ while(1)
             cout << "recv: " << str_recv << endl;
         } else {
             cout << "error: cannot read packet " << endl;
-            return  EXIT_FAILURE;
+            ret = CODE_ERROR;
+            break;
         }
 
-        str_send.resize(str_recv.size());
 
-        // toUpper
-        std::transform(str_recv.begin(), str_recv.end(), str_send.begin(), toUpper );
+        // create send data
+        str_send = str2upper(str_recv);
 
         packet_send.clear();
         packet_send << str_send;
 
+
         // send packet
-        status = client.send(packet_send);
+        status = (*socket).send(packet_send);
         if (status != sf::Socket::Done)
         {
             cout << "send Faild: "<< endl;
             printStatus(status);
-            return  EXIT_FAILURE;
+            ret = CODE_ERROR;
+            break;
         }
 
         cout << "send: " << str_send << endl;
@@ -134,7 +157,5 @@ while(1)
 
     } // while
 
-    } // while
-
-    return EXIT_SUCCESS;
+    return ret;
 }
