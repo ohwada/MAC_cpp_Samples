581642283571262db35825813baf1d0fdf399863
Kenichi Ohwada <ml.ohwada@gmail.com>
2020-08-08 09:09:29 +0900

    update

--- FileUtil/README.md
+++ FileUtil/README.md
@@ -6,7 +6,7 @@ C++ Sample <br/>
 ## read_write_text.cpp
 read and write Text File <br/>
 
-### build and run sample code 
+### build and excute sample code 
 % g++  read_write_text.cpp -o read_write_text -std=c++11 <br/>
 % ./read_write_text ./data/sample.txt output.txt <br/>
 
@@ -18,11 +18,11 @@ and show text  <br/>
 and write to output file  <br/>
  ex ) output.txt  <br/>
 
-## parse_filename.cpp
+## parse_filename.hpp
 parse file fullpath to dir name, file name, file extension <br/>
 
 ### build sample code
-% g++ test_parse_filename.cpp parse_filename.cpp -o test_parse_filename -std=c++11 <br/>
+% g++ test_parse_filename.cpp  -o test_parse_filename -std=c++11 <br/>
 
 ### excute sample code 
 % ./test_parse_filename  /etc/sample.txt <br/>
--- FileUtil/parse_filename.hpp
+++ FileUtil/parse_filename.hpp
@@ -3,7 +3,13 @@
  * 2020-02-01 K.OHWADA
  */
 
+// parse file fullpath to dir name, file name, file extension 
+
 #include <string> 
+
+
+#ifndef _H_PARSE_FILENAME_H_
+#define _H_PARSE_FILENAME_H_
 
 bool parseFileName(std::string fullpath, std::string &dir, std::string &title, std::string &ext);
 
@@ -18,3 +24,108 @@ void parseFilePath(std::string fullpath, std::string &dir, std::string &basename
 bool parseBaseName(std::string basename, std::string &title, std::string &ext)
 ;
 
+
+/**
+ * parseFilePath
+ */
+void parseFilePath(std::string fullpath, std::string &dir, std::string &basename)
+{
+    int pos  = fullpath.find_last_of('/');
+    if (pos == std::string::npos) {
+        dir = "";
+        basename = fullpath;
+    } else {
+        dir = fullpath.substr(0,pos+1);
+        basename = fullpath.substr(pos+1, fullpath.size()-pos);
+    }
+
+}
+
+
+/**
+ * parseBaseName
+ */
+bool parseBaseName(std::string basename, std::string &title, std::string &ext)
+{
+
+
+    int pos = basename.find('.');
+    if (pos == std::string::npos) {
+        return false;
+    }
+
+    title = basename.substr(0, pos);
+    ext = basename.substr(pos+1, basename.size()-pos);
+
+    return true;
+}
+
+
+/**
+ * parseFileName
+ */
+bool parseFileName(std::string fullpath, std::string &dir, std::string &title, std::string &ext)
+{
+
+    std::string basename;
+    parseFilePath(fullpath, dir, basename);
+
+    bool ret = parseBaseName(basename, title, ext);
+    if (!ret) {
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * getFileNameWithExt
+ */
+std::string getFileNameWithExt(std::string fullpath)
+{
+    std::string dir;
+    std::string basename;
+    parseFilePath(fullpath, dir, basename);
+    return basename;
+}
+
+
+/**
+ * getFileNameWithoutExt
+ */
+std::string getFileNameWithoutExt(std::string fullpath)
+{
+    std::string dir;
+    std::string basename;
+    parseFilePath(fullpath, dir, basename);
+
+    std::string title;
+    std::string ext;
+    bool ret = parseBaseName(basename, title, ext);
+if(!ret) {
+    title = basename;
+}
+    return title;
+}
+
+
+/**
+ * getFileNameExt
+ */
+std::string getFileNameExt(std::string fullpath)
+{
+    std::string dir;
+    std::string basename;
+    parseFilePath(fullpath, dir, basename);
+
+    std::string title;
+    std::string ext;
+    bool ret = parseBaseName(basename, title, ext);
+    if(!ret) {
+        ext = "";
+    }
+    return ext;
+}
+
+
+#endif // _H_PARSE_FILENAME_H_
