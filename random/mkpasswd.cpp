958bcf1332574311e75d256241a497ba21be5623
Kenichi Ohwada <ml.ohwada@gmail.com>
2022-12-20 02:26:03 +0900

    update

--- random/mkpasswd.cpp
+++ random/mkpasswd.cpp
@@ -1,7 +1,7 @@
 /**
- * random sample
+ * mkpasswd.cpp
  * 2022-06-01 K.OHWADA
  */
 
-// see
-// https://github.com/ohwada/MAC_cpp_Samples/blob/master/random/src_cpp/mkpasswd.cpp
+move to
+https://github.com/ohwada/MAC_cpp_Samples/blob/master/random/src_cpp/mkpasswd.cpp
