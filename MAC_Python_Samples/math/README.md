e3ea912d1377c665a289d514b03bdc211a44dc96
Kenichi Ohwada <ml.ohwada@gmail.com>
2025-05-05 22:15:45 +0900

    add math

---
+++ MAC_Python_Samples/math/README.md
@@ -0,0 +1,12 @@
+math
+===============
+
+Standard Library
+
+[ibrary math](https://docs.python.org/ja/3.13/library/math.html)  
+
+mathematics functions
+
+## samples
+
+- math_func.py  
---
+++ MAC_Python_Samples/math/math_func.py
@@ -0,0 +1,36 @@
+# Python: calc math functions
+# 2025-04-10  K.OHWADA
+
+# https://note.nkmk.me/python-gcd-lcm/#google_vignette
+
+import  math
+
+# constant
+print( 'pi: ', math.pi ) # 3.14
+
+print( 'e: ', math.e ) # 2.71
+
+print( 'tau: ', math.tau ) # 6.28
+
+# Greatest common divisor
+print('gcd(6, 4) = ', math.gcd(6, 4)) # 2
+
+# Least common multiple
+print('lcm(6, 4) = ', math.lcm(6, 4)) # 12
+
+# Angular conversion
+print()
+print('degrees(pi) = ', math.degrees(math.pi)) # 1180.0
+
+print('radians(180) = ', math.radians(180)) # 3.14
+
+# Trigonometric functions
+print()
+sin30 = math.sin(math.radians(30))
+print('sin(30 deg) = ', sin30) # 0.5
+
+cos30 = math.cos(math.radians(30))
+print('cos(30 deg) = ', cos30 ) # 0.866
+
+ret30 = (sin30 * sin30) + (cos30*cos30)
+print( 'sin(30 deg)**2 + cos(30 deg)**2 = ', ret30) # 1.0
