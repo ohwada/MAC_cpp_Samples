477aadfff8bacc996700c94908e391753fbb98dc
Kenichi Ohwada <ml.ohwada@gmail.com>
2025-06-24 23:16:14 +0900

    update

--- MAC_Python_Samples/pyqt/qt_version.py
+++
@@ -1,8 +0,0 @@
-# Python: Qt version
-# 2025-04-10  K.OHWADA
-
-#  https://retrofocus28.blogspot.com/2013/06/pyqt_25.html
-
-from PyQt5.QtCore import *
-
-print ('Qt version: ', QT_VERSION_STR) # 5.15.7
